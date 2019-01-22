368d914dcfb50ec279244e71140b83264adff5c0
Emmanuel Anzele <eanzele@gmail.com>
2019-01-22 22:47:14 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,7 +12,8 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-contacts["Freddy Mercury"].each do 
+contacts["Freddy Mercury"].each do |key,value|
+  
   #your code here
 
 
